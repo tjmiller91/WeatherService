594aa2876bcebeb712db3b94dd0983b9e6d31749
tjmiller91 <51676342+tjmiller91@users.noreply.github.com>
2023-09-07 22:50:29 -0500

    Add files via upload

---
+++ WeatherServiceProject/WeatherService/Implementations/WeatherForecastImplementation.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherService.Implementations
+{
+    public class WeatherForecastImplementation
+    {
+        internal static WeatherForecast GetWeatherForecast()
+        {
+            //todo: retreive current temperature from:
+            //https://www.weatherbit.io/api/swaggerui/weather-api-v2#!/Current32Weather32Data/get_current_postal_code_postal_code
+            //using API Key: 1824631bbfa74729aac7d2d2f1dfdd76
+
+            return null;
+        }
+    }
+}
